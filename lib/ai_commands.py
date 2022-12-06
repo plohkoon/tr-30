2f372a68675071b5ebcf0a133f83a06237e3d9ac
Greg Huber <gregoryh@uvic.ca>
2022-12-05 17:03:32 -0800

    Bunch of cleanup, render everything in embeds

--- lib/ai_commands.py
+++ lib/ai_commands.py
@@ -1,59 +1,13 @@
 from discord.app_commands import command, guilds
-from discord import Interaction
+from discord import Interaction, Embed, Colour
 from discord.ext.commands import Cog, Bot
-from .constants import TR_GUILD, IABW_GUILD
+from .constants import TR_GUILD, IABW_GUILD, IABW_LOGO
 from openai import Completion, Image, InvalidRequestError
 import sys
 
 
 # TODO: Update this to chat bot when I can get it working
 DEFAULT_MODEL = "text-davinci-003"
-
-MODEL_LIST_TEXT = f"""
-Here are the available models:
-
-**Davinci** (Default):
-
-Model Key: `text-davinci-003`
-
-```
-Davinci is the most capable model family and can perform any task the other models can perform and often with less instruction. For applications requiring a lot of understanding of the content, like summarization for a specific audience and creative content generation, Davinci is going to produce the best results. These increased capabilities require more compute resources, so Davinci costs more per API call and is not as fast as the other models.
-
-Good at: Complex intent, cause and effect, summarization for audience
-```
-
-**Curie**:
-
-Model Key: `text-curie-001`
-
-```
-Curie is extremely powerful, yet very fast. While Davinci is stronger when it comes to analyzing complicated text, Curie is quite capable for many nuanced tasks like sentiment classification and summarization. Curie is also quite good at answering questions and performing Q&A and as a general service chatbot.
-
-Good at: Language translation, complex classification, text sentiment, summarization
-```
-
-**Babbage**:
-
-Model Key: `text-babbage-001`
-
-```
-Babbage can perform straightforward tasks like simple classification. It’s also quite capable when it comes to Semantic Search ranking how well documents match up with search queries.
-
-Good at: Moderate classification, semantic search classification
-```
-
-**Ada**:
-
-Model Key: `text-ada-001`
-
-```
-Ada is usually the fastest model and can perform tasks like parsing text, address correction and certain kinds of classification tasks that don’t require too much nuance. Ada’s performance can often be improved by providing more context.
-
-Good at: Parsing text, simple classification, address correction, keywords
-```
-
-For more information see https://beta.openai.com/docs/models/finding-the-right-model
-"""
 
 MODEL_LIST = [
     "text-davinci-003",
@@ -80,11 +34,23 @@ class AICommands(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    @command(name="chat", description="Chat with the AI")
+    @command(name="prompt", description="Prompt with the AI")
     @guilds(TR_GUILD, IABW_GUILD)
-    async def chat(self, interaction: Interaction, prompt: str, model: str = DEFAULT_MODEL) -> None:
+    async def prompt(self, interaction: Interaction, prompt: str, model: str = DEFAULT_MODEL) -> None:
         if model not in MODEL_LIST:
-            await interaction.response.send_message("Model not found. To see available models use /list_models", ephemeral=True)
+            embed = Embed(
+                title="Error",
+                color=Colour.red(),
+                description="Something went wrong. Please try again later."
+            )
+
+            embed.add_field(
+                name="Message",
+                value="Model not found. To see available models user /list_models",
+                inline=False
+            )
+
+            await interaction.response.send_message(embed=embed, ephemeral=True)
             return
 
         await interaction.response.defer()
@@ -92,28 +58,132 @@ class AICommands(Cog):
         print(
             f"Generating a completion for the prompt: {prompt}", file=sys.stderr)
 
+        embed = None
+
         try:
             response = Completion.create(
                 model=model, prompt=prompt, max_tokens=MAX_TOKENS[model])
 
             message = response["choices"][0]["text"]
+
+            embed = Embed(color=Colour.green())
+
+            embed.set_thumbnail(url=IABW_LOGO)
+
+            embed.set_author(name=interaction.user.display_name,
+                             icon_url=interaction.user.avatar.url)
+
+            embed.add_field(
+                name="Prompt", value=prompt.capitalize(), inline=False)
+            embed.add_field(name="Model", value=model, inline=False)
+            embed.add_field(name="Response", value=message, inline=False)
+
+            embed.set_footer(text="Powered by OpenAI")
         except InvalidRequestError as e:
-            message = f"Error: {e._message}"
-        except:
-            message = "An unknown error occurred. Please try again later."
+            embed = Embed(
+                title="Error",
+                color=Colour.red(),
+                description="Something went wrong. Please try again later."
+            )
+
+            embed.add_field(name="Message", value=e._message, inline=False)
+        except Exception as e:
+            embed = Embed(
+                title="Error",
+                color=Colour.red(),
+                description="An unknown error occurred. Please try again later."
+            )
+
+            embed.add_field(name="message", value=str(e))
         finally:
-            await interaction.followup.send(message)
+            await interaction.followup.send(embed=embed)
 
     @command(name="list_models", description="List the available models")
     @guilds(TR_GUILD, IABW_GUILD)
     async def list_models(self, interaction: Interaction) -> None:
-        await interaction.response.send_message(MODEL_LIST_TEXT, ephemeral=True)
+        print("Listing models for", interaction.user.display_name, file=sys.stderr)
+        embed = Embed(
+            title="Available Models",
+            color=Colour.green(),
+            description="There is a wide variety of models available."
+        )
+
+        embed.add_field(
+            name="Default Model",
+            inline=False,
+            value=f"`{DEFAULT_MODEL}`",
+        )
+
+        embed.add_field(
+            name="Davinci",
+            inline=False,
+            value="""
+Key: `text-davinci-003`
+
+Davinci is the most capable model family and can perform any task the other models can perform and often with less instruction. For applications requiring a lot of understanding of the content, like summarization for a specific audience and creative content generation, Davinci is going to produce the best results. These increased capabilities require more compute resources, so Davinci costs more per API call and is not as fast as the other models.
+
+Good at: Complex intent, cause and effect, summarization for audience
+"""
+        )
+
+        embed.add_field(
+            name="Curie",
+            inline=False,
+            value="""
+Key: `text-curie-001`
+
+Curie is extremely powerful, yet very fast. While Davinci is stronger when it comes to analyzing complicated text, Curie is quite capable for many nuanced tasks like sentiment classification and summarization. Curie is also quite good at answering questions and performing Q&A and as a general service chatbot.
+
+Good at: Language translation, complex classification, text sentiment, summarization
+"""
+        )
+
+        embed.add_field(
+            name="Babbage",
+            inline=False,
+            value="""
+Key: `text-babbage-001`
+
+Babbage can perform straightforward tasks like simple classification. It’s also quite capable when it comes to Semantic Search ranking how well documents match up with search queries.
+
+Good at: Moderate classification, semantic search classification
+"""
+        )
+
+        embed.add_field(
+            name="Ada",
+            inline=False,
+            value="""
+Key: `text-ada-001`
+
+Ada is usually the fastest model and can perform tasks like parsing text, address correction and certain kinds of classification tasks that don’t require too much nuance. Ada’s performance can often be improved by providing more context.
+
+Good at: Parsing text, simple classification, address correction, keywords
+"""
+        )
+
+        embed.set_footer(
+            text="Powered by OpenAI. For more information see https://beta.openai.com/docs/models/finding-the-right-model")
+
+        await interaction.response.send_message(embed=embed, ephemeral=True)
 
     @command(name="visualize", description="Make an image")
     @guilds(TR_GUILD, IABW_GUILD)
     async def visualize(self, interaction: Interaction, prompt: str, size: str = "medium") -> None:
         if size not in IMAGE_SIZES:
-            await interaction.response.send_message("Invalid image size. Use one of: small, medium, large", ephemeral=True)
+            embed = Embed(
+                title="Error",
+                color=Colour.red(),
+                description="Something went wrong. Please try again later."
+            )
+
+            embed.add_field(
+                name="Message",
+                value="Model not found. Invalid image size. Use one of: small, medium, large",
+                inline=False
+            )
+
+            await interaction.response.send_message(embed=embed, ephemeral=True)
             return
 
         await interaction.response.defer()
@@ -121,13 +191,40 @@ class AICommands(Cog):
         print(
             f"Generating an image according to the prompt: {prompt}", file=sys.stderr)
 
+        embed = None
+
         try:
             response = Image.create(
                 prompt=prompt, size=IMAGE_SIZES[size], n=1, response_format="url", user=f"{interaction.user.id}")
-            message = response["data"][0]["url"]
+            embed = Embed(color=Colour.green())
+
+            embed.set_thumbnail(url=IABW_LOGO)
+
+            embed.set_author(name=interaction.user.display_name,
+                             icon_url=interaction.user.avatar.url)
+
+            embed.add_field(
+                name="Prompt", value=prompt.capitalize(), inline=False)
+            embed.add_field(name="Size", value=size.capitalize(), inline=False)
+
+            embed.set_image(url=response["data"][0]["url"])
+
+            embed.set_footer(text="Powered by OpenAI")
         except InvalidRequestError as e:
-            message = f"Error: {e._message}"
-        except:
-            message = "An unknown error occurred. Please try again later."
+            embed = Embed(
+                title="Error",
+                color=Colour.red(),
+                description="Something went wrong. Please try again later."
+            )
+
+            embed.add_field(name="Message", value=e._message, inline=False)
+        except Exception as e:
+            embed = Embed(
+                title="Error",
+                color=Colour.red(),
+                description="An unknown error occurred. Please try again later."
+            )
+
+            embed.add_field(name="message", value=str(e))
         finally:
-            await interaction.followup.send(message)
+            await interaction.followup.send(embed=embed)
